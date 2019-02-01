a1e0630a99fb4c56d7b9129b213eb1b51bcf95d8
Admin <sean@decrypteddesign.com>
2019-02-01 15:23:32 -0800

    tpye: feat | first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	ob_start();
+	session_start();
+
+	$timezone = date_default_timezone_set("America/Los_Angeles");
+
+	$con = mysqli_connect("localhost", "root", "root", "task_manager");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+
+?>
