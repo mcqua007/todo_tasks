57469ccc0f2e51913307ebafa57bc0d0764c33e3
Admin <sean@decrypteddesign.com>
2019-02-02 17:05:50 -0800

    tpye: feat | fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 $(function(){
    //process the form for task manager
+
+   $( "#todoList" ).sortable();
+    $( "#todoList" ).disableSelection();
+
    $("#task_form").submit(function(event){
      // stop the form from submitting the normal way and refreshing the page
       event.preventDefault();
       //grab form data
       var formData = {
-        'name' : $('#name_input').val(),
+        'title' : $('#title_input').val(),
         'description' : $('#description_input').val(),
         'severity' : $('#severity_input').val(),
         'assigned' : $('#assigned_to_input').val()
@@ -16,10 +20,36 @@ $(function(){
       $.ajax({
         type: 'POST',
         url: 'includes/rest/process.php',
-        data: formData,
-        success: function() {alert("success!"); }
+        data: formData
+        //success: function() {alert("success!"); }
       }).done(function(data) {
         console.log(data); //test if data is being called back
+        //return same data entered in JSON, use data to buld a card, that shows up on the page
+        //build card in javascript
+         var response = JSON.parse(data);
+         console.log(response.severity);
+
+
+        var htmlBadge = "";
+
+
+        if (response.severity == "low"){
+          htmlBadge = "<span class='badge  badge-success' style='float:right; width:20%; padding:5px; '>Low</span>";
+        }else if(response.severity == "medium"){
+          htmlBadge = "<span class='badge  badge-warning' style='float:right; width:20%; padding:5px; '>Medium</span>";
+        }
+        else {
+        htmlBadge =" <span class='badge  badge-danger' style='float:right; width:20%; padding:5px; '>High</span>";
+        }
+        htmlInput = "<div class='input-group mb-3'  style='padding:10px;'><input type='text' class='form-control' placeholder='To do' aria-label='Recipient's username' aria-describedby='button-addon2'><div class='input-group-append'><button class='btn btn-outline-secondary' type='button' id='todo-button-" + response.id +"'><i class='fa fa-plus'></i></button></div></div>";
+
+        var html ="<div class='col-sm-12 col-md-3 col-xl-4 animated fadeInRight' id='todo-card-"+ response.id +"' style='margin-top:10px;' data-id='"+ response.id +"'><div class='col-xs-12 card'><div style='width:100%; padding:10px;'><span class='card-title' style='width:70%; margin:10px; font-weight:700; font-size:16px; text-transform:uppercase;'>" + response.title + "</span>" + htmlBadge + "</div><div class='card-body'><p class='card-subtitle mb-2 text-muted'>"+ response.description + "</p><div id='todo-" + response.id +"'></div></div>" + htmlInput + "</div></div>";
+        $("#todoList").append(html);
+
+        setTimeout(function(){
+          console.log("in here");
+          $("#todo-card-"+ response.id).removeClass("fadeInRight");
+        }, 900);
       });
 
 
--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Task Tracker</title>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" />
   </head>
