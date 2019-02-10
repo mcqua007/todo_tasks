a3ddf99541c4b62abf354f0f72314a247601285f
Admin <sean@decrypteddesign.com>
2019-02-10 10:44:08 -0800

    type: feat   |added menu open and close rotating animation

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -1,22 +1,36 @@
 function openSide(){
-  $( "#menu-icon" ).hide();
-  $( "#menu-icon-close" ).show();
+  $( "#menu-icon-close" ).removeClass("menu-close-animation");
+  $( "#menu-icon" ).addClass("menu-animation");
+
+  setTimeout(function(){
+    $( "#menu-icon" ).hide();
+    $( "#menu-icon-close" ).show();
+  }, 300);
+
   $( ".sidebar-nav-closed" ).animate({
      width: "15%",
      opacity: 1,
    }, 600 );
+
      $( "#menu-list" ).show("slow");
 
 }
 
 function closeSide(){
+  $( "#menu-icon" ).removeClass("menu-animation");
+  $( "#menu-icon-close" ).addClass("menu-close-animation");
 
-  $( "#menu-icon-close" ).hide();
+  setTimeout(function(){
+    $( "#menu-icon-close" ).hide();
+    $( "#menu-icon" ).show();
+  }, 300);
+
+
   $( ".sidebar-nav-closed" ).animate({
      width: "0px",
      opacity: 1,
    }, 600 );
      $( "#menu-list" ).hide();
-       $( "#menu-icon" ).show();
+
 
 }
--- includes/topbar.php
+++ includes/topbar.php
@@ -1,17 +1,11 @@
 
 
-<div class="" style="
-    background-color: #007bff;
-    position: fixed;
-    width: 100%;
-    z-index: 1;
-    top:0px;
-">
+<div class="" style="background-color: #007bff; position: fixed; width: 100%; z-index: 99999; top:0px;">
 <div ><i class="fa fa-times" style="display:none;  color: #fff; padding-top: 7px; padding-left: 6px; margin-bottom: -5px; font-size: 21px" id="menu-icon-close" onclick="closeSide()"></i>
 <i onclick="openSide()" class="fa fa-bars" style="color: #fff; padding-top: 7px; padding-left: 6px; margin-bottom: -5px; font-size: 21px;" id="menu-icon" ></i></div>
-
     <center><h3 class="logo-font" style="margin-top: -30px;color: #fff; font-family: 'Leckerli One', cursive; font-size: 25px;">Staqpack</h3></center>
-
-
-      <button class="btn btn-primary" onclick="showTaskManager()" style="float:right; padding-right:10px; float: right; padding-right: 10px; position: relative; margin-top: -40px;"><i class="fa fa-plus  m-right-5" id="add-task-icon" style="color:#fff;"></i><span class="add-task-text" style="color:#fff;"> Add Task </span></button>
+      <a class="btn btn-primary" onclick="showTaskManager()" style="float:right; padding-right:10px; float: right; padding-right: 10px; position: relative; margin-top: -40px;">
+        <i class="fa fa-plus  m-right-5" id="add-task-icon" style="color:#fff;"></i>
+        <span class="add-task-text" style="color:#fff;"> Add Task </span>
+      </a>
 </div>
