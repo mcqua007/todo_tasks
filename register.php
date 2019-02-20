3ab9510984e37157d90a1da06eb1a03a3a836527
Admin <sean@decrypteddesign.com>
2019-02-19 19:43:20 -0800

    type: feat | reorganized the layout of login fo rmobile

--- register.php
+++ register.php
@@ -12,10 +12,12 @@ include("includes/header.php");
 <head>
 	<title>Welcome to Staqpack</title>
 
-	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Leckerli+One|Lobster+Two|Pacifico" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="assets/css/register.css">
 	<script src="assets/js/register.js"></script>
 </head>
 <body>
@@ -41,101 +43,102 @@ include("includes/header.php");
 	?>
 
 
-	<div id="background">
+	<div  id="background">
+		<div class="container">
+			<div class="row" id="loginContainer">
 
-		<div id="loginContainer">
+				<div class="col-12 col-md-6" id="inputContainer">
+					<form id="loginForm" action="register.php" method="POST">
+						<h2>Login</h2>
+						<p>
+							<?php echo $account->getError(Constants::$loginFailed); ?>
+							<label for="loginUsername">Username</label>
+							<input id="loginUsername" name="loginUsername" type="text" placeholder="Your username" value="<?php getInputValue('loginUsername') ?>" required>
+						</p>
+						<p>
+							<label for="loginPassword">Password</label>
+							<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+						</p>
 
-			<div id="inputContainer">
-				<form id="loginForm" action="register.php" method="POST">
-					<h2>Login</h2>
-					<p>
-						<?php echo $account->getError(Constants::$loginFailed); ?>
-						<label for="loginUsername">Username</label>
-						<input id="loginUsername" name="loginUsername" type="text" placeholder="Your username" value="<?php getInputValue('loginUsername') ?>" required>
-					</p>
-					<p>
-						<label for="loginPassword">Password</label>
-						<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
-					</p>
+						<button  class="btn btn-primary" type="submit" name="loginButton">LOG IN</button>
 
-					<button  class="btn btn-primary" type="submit" name="loginButton">LOG IN</button>
+						<div class="hasAccountText">
+							<span id="hideLogin">Don't have an account yet? Signup here.</span>
+						</div>
 
-					<div class="hasAccountText">
-						<span id="hideLogin">Don't have an account yet? Signup here.</span>
-					</div>
-
-				</form>
+					</form>
 
 
 
-				<form id="registerForm" action="register.php" method="POST">
-					<h2>Create your free account</h2>
-					<p>
-						<?php echo $account->getError(Constants::$usernameCharacters); ?>
-						<?php echo $account->getError(Constants::$usernameTaken); ?>
-						<label for="username">Username</label>
-						<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
-					</p>
+					<form id="registerForm" action="register.php" method="POST">
+						<h2>Create your free account</h2>
+						<p>
+							<?php echo $account->getError(Constants::$usernameCharacters); ?>
+							<?php echo $account->getError(Constants::$usernameTaken); ?>
+							<label for="username">Username</label>
+							<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+						</p>
 
-					<p>
-						<?php echo $account->getError(Constants::$firstNameCharacters); ?>
-						<label for="firstName">First name</label>
-						<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
-					</p>
+						<p>
+							<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+							<label for="firstName">First name</label>
+							<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+						</p>
 
-					<p>
-						<?php echo $account->getError(Constants::$lastNameCharacters); ?>
-						<label for="lastName">Last name</label>
-						<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
-					</p>
+						<p>
+							<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+							<label for="lastName">Last name</label>
+							<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+						</p>
 
-					<p>
-						<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-						<?php echo $account->getError(Constants::$emailInvalid); ?>
-						<?php echo $account->getError(Constants::$emailTaken); ?>
-						<label for="email">Email</label>
-						<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
-					</p>
+						<p>
+							<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+							<?php echo $account->getError(Constants::$emailInvalid); ?>
+							<?php echo $account->getError(Constants::$emailTaken); ?>
+							<label for="email">Email</label>
+							<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+						</p>
 
-					<p>
-						<label for="email2">Confirm email</label>
-						<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
-					</p>
+						<p>
+							<label for="email2">Confirm email</label>
+							<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+						</p>
 
-					<p>
-						<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
-						<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-						<?php echo $account->getError(Constants::$passwordCharacters); ?>
-						<label for="password">Password</label>
-						<input id="password" name="password" type="password" placeholder="Your password" required>
-					</p>
+						<p>
+							<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+							<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+							<?php echo $account->getError(Constants::$passwordCharacters); ?>
+							<label for="password">Password</label>
+							<input id="password" name="password" type="password" placeholder="Your password" required>
+						</p>
 
-					<p>
-						<label for="password2">Confirm password</label>
-						<input id="password2" name="password2" type="password" placeholder="Your password" required>
-					</p>
+						<p>
+							<label for="password2">Confirm password</label>
+							<input id="password2" name="password2" type="password" placeholder="Your password" required>
+						</p>
 
-					<button type="submit" name="registerButton">SIGN UP</button>
+						<button type="submit" name="registerButton">SIGN UP</button>
 
-					<div class="hasAccountText">
-						<span id="hideRegister">Already have an account? Log in here.</span>
-					</div>
+						<div class="hasAccountText">
+							<span id="hideRegister">Already have an account? Log in here.</span>
+						</div>
 
-				</form>
+					</form>
 
+
+				</div>
+
+				<div class="col-12 col-md-6" id="loginText">
+					<h1 style="font-family: 'Leckerli One', cursive; ">Staqpack</h1>
+					<h2>If you think about it, Just do it!</h2>
+					<ul>
+						<li>Organize Projects</li>
+						<li>Create tasks</li>
+						<li>Complete To Dos to follow through</li>
+					</ul>
+				</div>
 
 			</div>
-
-			<div id="loginText">
-				<h1 style="font-family: 'Leckerli One', cursive; ">Staqpack</h1>
-				<h2>If you think about it, Just do it!</h2>
-				<ul>
-					<li>Organize Projects</li>
-					<li>Create tasks</li>
-					<li>Complete To Dos to follow through</li>
-				</ul>
-			</div>
-
 		</div>
 	</div>
 
