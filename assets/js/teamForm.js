188a3e142cb942148f3ab3c8ef8b7bdb27d2bc56
mcqua007 <mcqua007@cougars.csusm.edu>
2019-03-28 10:34:46 -0700

    type: feat | backedn add team code

--- assets/js/teamForm.js
+++ assets/js/teamForm.js
@@ -6,7 +6,7 @@ $(function(){
 
      console.log("# members: " + num);
      for(i = 1; i <= num; i++){
-       var numHtml = "<div class='form-group col-sm-6 col-md-4 col-lg-3' id=''><label for='teamMemberInput'>Team Member "+ i +"</label><input type='text' class='form-control team-field' id='members' placeholder='Enter Username'></div>";
+       var numHtml = "<div class='form-group col-sm-6 col-md-4 col-lg-3' id=''><label for='teamMemberInput'>Team Member "+ i +"</label><input type='text' class='form-control team-field' id='member_"+i+"' placeholder='Enter Username'></div>";
        $("#add_inputs").append(numHtml);
      }
   });
@@ -15,19 +15,31 @@ $(function(){
   // SUBMIT PROJECT FORM - process form data             ===
   //=====================================================
      $("#team_form").submit(function(event){
-       // stop the form from submitting the normal way and refreshing the page
-         var formData = $("#team_form").serializeArray();
-        event.preventDefault();
+         // stop the form from submitting the normal way and refreshing the page
+         event.preventDefault();
 
-        
+         var name = $("#team_name").val();
+         var num = $("#num_members").val(); // reset the inputs
+         var member1 = $("#member_1").val();
+         var member2 = $("#member_2").val();
+         var member3 = $("#member_3").val();
+         var member4 = $("#member_4").val();
+         var member5 = $("#member_5").val();
 
-        var data = {};
-        $(formData).each(function(index, obj){
-            data[obj.name] = obj.value;
-        });
+         var formData = {
+           'team_name': name,
+           'number': num,
+           'member1': member1,
+           'member2': member2,
+           'member3': member3,
+           'member4': member4,
+           'member5': member5
+         }
+
+
+
 
         console.log(formData);
-        console.log(data);
 
 
       $.ajax({
--- includes/rest/processTeamForm.php
+++ includes/rest/processTeamForm.php
@@ -28,10 +28,8 @@ else{
 
     if (empty($_POST['team_name']))
         $errors['team_name'] = 'Team is required.';
-
-
-
-
+		if (empty($_POST['number']))
+		        $errors['num'] = 'Number of membrs is required.';
 // return a response ===========================================================
 
 // if there are any errors in errors array, return a success boolean of false
@@ -41,48 +39,364 @@ else{
         $data['errors']  = $errors;
     } else {
 
-        // if there are no errors process the form, then return a message
+			$num = $_POST['number'];
+			$team_name = $_POST['team_name'];
+			$date = date("Y-m-d H:i:s");
 
-     //  $title = $_POST['title'];
-     //  $type = $_POST['type'];
-		 //
-     //  $date = date("Y-m-d H:i:s");
-		 //
-		 //
-		 //
-		 //
-     //  $projectQuery = mysqli_query($con, "INSERT INTO projects VALUES(NULL, '$title', '$description','$user_id', '0', '$date', '0')");
-			// $lastId = mysqli_insert_id($con);
-		 //
-			// if($projectQuery == false) {
-     //      $data['sql_error'] = "Query failed: " . mysqli_error($con);
-     //  }
-		 //
-		 //
-     // //if type of project is team then insert into teamProjects table else insert into userProjects table
-     //  if($_POST['type'] == "team"){
-     //     $team_id = $_POST['team'];
-     //     $projectTeamQuery = mysqli_query($con, "INSERT INTO teamProjects VALUES(NULL, '$team_id', '$lastId')");
-     //  }
-     //  else if($_POST['type'] == "personal"){
-     //    $projectTeamQuery = mysqli_query($con, "INSERT INTO userProjects VALUES(NULL, '$user_id', '$lastId')");
-     //  }
-		 //
-     //  $getProject = mysqli_query($con, "SELECT id, title FROM projects WHERE id ='$lastId'");
-		 //
-			// while($row = mysqli_fetch_array($getProject)){
-			// 	$project['id'] = $row['id'];
-			// 	$project['title'] = $row['title'];
-		 //
-			// }
+			switch($num){
 
-      // show a message of success and provide a true success variable
-      $data['success'] = true;
-      $data['message'] = 'Success!';
+				case 1:
 
-			$data['team_name'] = $_POST['team_name'];
+				 $member1 = $_POST['member1'];
+
+				 $teamQuery = mysqli_query($con, "INSERT INTO teams VALUES(NULL, '$team_name', '1', '$date')");
+				 $lastId = mysqli_insert_id($con);
+
+				 $getMember1 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member1'");
+				 if($getMember1 == true){
+
+						 while($row == mysqli_fetch_array($getMember1)){
+							  $memberId1 = $row['id'];
+						  }
+
+					  //insert team member 1 into teamUsers table
+					  $teamUser1 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId1')");
+
+					 //insert logged in user into teamUsers
+					 $teamUserLoggedIn = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$user_id')");
+				 }
+				 else{
+					 $errors['member1'] = 'Team member 1 username is incorrect!';
+				 }
 
 
+				break;
+
+				case 2:
+
+					$member1 = $_POST['member1'];
+					$member2 = $_POST['member2'];
+
+					//insert team name to teams
+					$teamQuery = mysqli_query($con, "INSERT INTO teams VALUES(NULL, '$team_name', '1', '$date')");
+ 				  $lastId = mysqli_insert_id($con);
+
+          //MEMBER 1 SECTION
+					$getMember1 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member1'");
+					if($getMember1 == true){
+
+							while($row = mysqli_fetch_array($getMember1)){
+								 $memberId1 = $row['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser1 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId1')");
+
+					}
+					 else{
+					 	$errors['member1'] = 'Team member 1 username is incorrect!';
+						break;
+				 	}
+					//END MEMBER 1
+
+					//MEMBER 2 SECTION
+					$getMember2 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member2'");
+					if($getMember2 == true){
+
+							while($row2 = mysqli_fetch_array($getMember2)){
+								 $memberId2 = $row2['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser2 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId2')");
+
+						 //insert logged in user into teamUsers
+						 $teamUserLoggedIn = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$user_id')");
+
+					}
+					 else{
+					 	$errors['member2'] = 'Team member 2 username is incorrect!';
+						break;
+				 	}
+					//END MEMBER 2
+
+
+				break;
+
+				case 3:
+
+					$member1 = $_POST['member1'];
+					$member2 = $_POST['member2'];
+					$member3 = $_POST['member3'];
+
+					//MEMBER 1 SECTION
+					$getMember1 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member1'");
+					if($getMember1 == true){
+
+							while($row = mysqli_fetch_array($getMember1)){
+								 $memberId1 = $row['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser1 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId1')");
+
+					}
+					 else{
+						$errors['member1'] = 'Team member 1 username is incorrect!';
+						break;
+					}
+					//END MEMBER 1
+
+					//MEMBER 2 SECTION
+					$getMember2 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member2'");
+					if($getMember2 == true){
+
+							while($row2 = mysqli_fetch_array($getMember2)){
+								 $memberId2 = $row2['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser2 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId2')");
+
+					}
+					 else{
+						$errors['member2'] = 'Team member 2 username is incorrect!';
+						break;
+					}
+					//END MEMBER 2
+
+					//MEMBER 3 SECTION
+					$getMember3 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member3'");
+					if($getMember3 == true){
+
+							while($row3 = mysqli_fetch_array($getMember3)){
+								 $memberId3 = $row3['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser3 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId3')");
+
+						 //insert logged in user into teamUsers
+						 $teamUserLoggedIn = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$user_id')");
+
+					}
+					 else{
+						$errors['member3'] = 'Team member 3 username is incorrect!';
+						break;
+					}
+					//END MEMBER 3
+
+				break;
+
+				case 4:
+
+					$member1 = $_POST['member1'];
+					$member2 = $_POST['member2'];
+					$member3 = $_POST['member3'];
+					$member4 = $_POST['member4'];
+
+
+					//MEMBER 1 SECTION
+					$getMember1 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member1'");
+					if($getMember1 == true){
+
+							while($row == mysqli_fetch_array($getMember1)){
+								 $memberId1 = $row['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser1 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId1')");
+
+					}
+					 else{
+						$errors['member1'] = 'Team member 1 username is incorrect!';
+						break;
+					}
+					//END MEMBER 1
+
+					//MEMBER 2 SECTION
+					$getMember2 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member2'");
+					if($getMember2 == true){
+
+							while($row2 = mysqli_fetch_array($getMember2)){
+								 $memberId2 = $row2['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser2 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId2')");
+
+
+
+					}
+					 else{
+						$errors['member2'] = 'Team member 2 username is incorrect!';
+						break;
+					}
+					//END MEMBER 2
+
+					//MEMBER 3 SECTION
+					$getMember3 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member3'");
+					if($getMember3 == true){
+
+							while($row3 = mysqli_fetch_array($getMember3)){
+								 $memberId3 = $row3['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser3 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId3')");
+
+					}
+					 else{
+						$errors['member3'] = 'Team member 3 username is incorrect!';
+						break;
+					}
+					//END MEMBER 3
+
+					//MEMBER 4 SECTION
+					$getMember4 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member4'");
+					if($getMember4 == true){
+
+							while($row4 = mysqli_fetch_array($getMember4)){
+								 $memberId4 = $row4['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser4 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId4')");
+
+						 //insert logged in user into teamUsers
+						 $teamUserLoggedIn = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$user_id')");
+
+					}
+					 else{
+						$errors['member4'] = 'Team member 4 username is incorrect!';
+						break;
+					}
+					//END MEMBER 4
+
+				break;
+
+				case 5:
+
+					$member1 = $_POST['member1'];
+					$member2 = $_POST['member2'];
+					$member3 = $_POST['member3'];
+					$member4 = $_POST['member4'];
+					$member5 = $_POST['member5'];
+
+					//MEMBER 1 SECTION
+					$getMember1 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member1'");
+					if($getMember1 == true){
+
+							while($row = mysqli_fetch_array($getMember1)){
+								 $memberId1 = $row['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser1 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId1')");
+
+					}
+					 else{
+						$errors['member1'] = 'Team member 1 username is incorrect!';
+						break;
+					}
+					//END MEMBER 1
+
+					//MEMBER 2 SECTION
+					$getMember2 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member2'");
+					if($getMember2 == true){
+
+							while($row2 = mysqli_fetch_array($getMember2)){
+								 $memberId2 = $row2['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser2 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId2')");
+
+					}
+					 else{
+						$errors['member2'] = 'Team member 2 username is incorrect!';
+						break;
+					}
+					//END MEMBER 2
+
+					//MEMBER 3 SECTION
+					$getMember3 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member3'");
+					if($getMember3 == true){
+
+							while($row3 = mysqli_fetch_array($getMember3)){
+								 $memberId3 = $row3['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser3 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId3')");
+
+					}
+					 else{
+						$errors['member3'] = 'Team member 3 username is incorrect!';
+						break;
+					}
+					//END MEMBER 3
+
+					//MEMBER 4 SECTION
+					$getMember4 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member4'");
+					if($getMember4 == true){
+
+							while($row4 = mysqli_fetch_array($getMember4)){
+								 $memberId4 = $row4['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser4 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId4')");
+
+					}
+					 else{
+						$errors['member4'] = 'Team member 4 username is incorrect!';
+						break;
+					}
+					//END MEMBER 4
+
+					//MEMBER 5 SECTION
+					$getMember5 = mysqli_query($con, "SELECT id  FROM users WHERE username ='$member5'");
+					if($getMember5 == true){
+
+							while($row5 = mysqli_fetch_array($getMember5)){
+								 $memberId5 = $row5['id'];
+							 }
+
+						 //insert team member 1 into teamUsers table
+						 $teamUser5 = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$memberId5')");
+
+						 //insert logged in user into teamUsers
+						 $teamUserLoggedIn = mysqli_query($con, "INSERT INTO teamUsers VALUES(NULL, '$lastId', '$user_id')");
+
+					}
+					 else{
+						$errors['member5'] = 'Team member 5 username is incorrect!';
+						break;
+					}
+					//END MEMBER 5
+
+				break;
+
+				default:
+			  	$errors['switch'] = 'Switch did not work';
+			}
+
+			if (!empty($errors)) {
+					// if there are items in our errors array, return those errors
+					$data['success'] = false;
+					$data['errors']  = $errors;
+			}else{
+				// if there are no errors process form
+
+	      // show a message of success and provide a true success variable
+	      $data['success'] = true;
+	      $data['message'] = 'Success!';
+
+				$data['team_name'] = $_POST['team_name'];
+				$data['team_id'] = $lastId;
+
+			}
     }
 
     // return all our data to an AJAX call
